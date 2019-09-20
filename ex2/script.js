6906534a597b55a2afa0c6d36f3a81af30b35ed1
Sergio <sergio.gomezdamas@gmail.com>
2019-09-20 15:54:46 +0200

    tiding up directories

--- ex2/script.js
+++
@@ -1,6 +0,0 @@
-
-function myfunction() {
-    document.getElementById('nom').innerHTML='Sergio';
-    document.getElementById('cnom').innerHTML='Gomez';
-    document.getElementById('birthday').innerHTML='01/01/2002';
-}
