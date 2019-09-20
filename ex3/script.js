d15223e249ed3ab0e82126988bdbdec08a621b2d
Sergio <sergio.gomezdamas@gmail.com>
2019-09-20 15:53:57 +0200

    joining htmls

--- ex3/script.js
+++ ex3/script.js
@@ -1,11 +1,11 @@
 aficions = ['Programacio', 'Natacio', 'Series', 'Ciencia', 'IA']
 
 function myfunction() {
-    var name = aficions;
+    var names = aficions;
     var theName = name.value;
     names.push(theName);
     document.getElementById("nom").innerHTML = "";
-    for (var I = 0; I < names.length; I++) {
+    for (var I = 0; I < names.length-1; I++) {
         nameList = "<li>" + names[I] + "</li>";
         document.getElementById("nom").innerHTML += nameList;
     }
