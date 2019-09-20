c8ace38ff4449e0685fcca02b0ebb28d94ea0c38
Sergio <sergio.gomezdamas@gmail.com>
2019-09-20 16:26:16 +0200

    fi activitat 3

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function myfunction() {
+function myfunction1() {
     var prom='Darwin';
     alert(prom);
     console.log(prom);
     document.getElementById('demo').innerHTML=prom;
+}
+aficions = ['Programacio', 'Natacio', 'Series', 'Ciencia', 'IA']
+
+function myfunction3() {
+    aficions.forEach(function(element){
+        myhtml=document.getElementById('aficions').innerHTML;
+        document.getElementById('aficions').innerHTML=myhtml+'<li>'+element+'</li>\n';
+        console.log(myhtml)
+
+    })
+}
+
+function myfunction2() {
+    document.getElementById('nom').innerHTML='Sergio';
+    document.getElementById('cnom').innerHTML='Gomez';
+    document.getElementById('birthday').innerHTML='01/01/2002';
 }
